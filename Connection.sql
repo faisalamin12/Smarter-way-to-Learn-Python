28b654e965c62743bfeacbf04177c7d6fdbea9ab
Faisal amin <36323184+faisalamin12@users.noreply.github.com>
2020-03-04 23:50:17 -0800

    Python Database Connection (My sql)

---
+++ Connection.sql
@@ -0,0 +1,13 @@
+create database Diabetes;
+use Diabetes;
+create table Info(
+Pregnancies int,
+Glucose int,
+BloodPressure int,
+Insulin int,
+BMI int,
+DiabetesPedigreeFunction float,
+Age int,
+Outcome int)
+
+select * from Info;
